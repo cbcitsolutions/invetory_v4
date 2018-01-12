b5323d18e2c17eb7707c0e92c528f923ce8c4667
Kalana Sarange <Kalana Sarange@Kalana-PC>
2018-01-12 15:05:45 +0530

    sachith Commit

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -18,6 +18,8 @@ public class JavaApplication8 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("madushanka");
+        System.out.println("SachithMadushan");
+        
     }
     
 }
